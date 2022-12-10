88054c716185c10037879d5d2235d19031f44129
Adrian Ordonez <adrianord@gmail.com>
2022-12-09 21:43:20 -0600

    feat(modules/programs/pulumi): add extra packages and variables

--- modules/programs/pulumi.nix
+++ modules/programs/pulumi.nix
@@ -12,9 +12,16 @@ in
   };
 
   config = mkIf cfg.enable {
-    home._.home.packages = with pkgs; [
-      pulumi-bin
-      pulumictl
-    ];
+    home._.home = {
+      sessionVariables = {
+        PULUMI_AUTOMATION_API_SKIP_VERSION_CHECK = true;
+        PULUMI_SKIP_UPDATE_CHECK = true;
+      };
+      packages = with pkgs; [
+        pulumi-bin
+        pulumictl
+        tf2pulumi
+      ];
+    };
   };
 }
