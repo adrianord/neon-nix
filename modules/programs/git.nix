85b818c4d661063f5e1ddd3f8e0b27cc5e177408
Adrian Ordonez <adrianord@gmail.com>
2023-01-18 12:11:11 -0600

    feat(programs/git): add gh and act

--- modules/programs/git.nix
+++ modules/programs/git.nix
@@ -50,51 +50,57 @@ in
   };
 
   config = mkIf cfg.enable {
-    home._.programs.git = {
-      enable = true;
-      userName = userConf.user.fullname;
-      userEmail = userConf.user.email;
-      aliases = {
-        a = "add";
-        b = "branch";
-        c = "commit";
-        d = "diff";
-        l = "log";
-        m = "merge";
-        r = "rebase";
-        s = "status";
-        co = "checkout";
-        dt = "difftool";
-        ps = "push";
-        pl = "pull";
-        lol = "log --oneline";
-        amend = "commit --no-edit --amend";
-        cl = "clone";
-      };
-      extraConfig = {
-        pull = {
-          rebase = true;
-        };
-        push = {
-          autoSetupRemote = true;
-        };
-        init = {
-          defaultBranch = "main";
-        };
-      };
-      delta = {
+    home._ = {
+      home.packages = with pkgs; [
+        gh
+        act
+      ];
+      programs.git = {
         enable = true;
-        options = {
-          features = "side-by-side line-numbers decorations";
-          whitespace-error-style = "22 reverse";
-          syntax-theme = "Monokai Extended";
-          navigation = true;
-          plus-style = "syntax #012800";
-          minus-style = "syntax #340001";
-          decorations = {
-            commit-decoration-style = "bold yellow box ul";
-            file-style = "bold yellow ul";
-            file-decoration-style = "none";
+        userName = userConf.user.fullname;
+        userEmail = userConf.user.email;
+        aliases = {
+          a = "add";
+          b = "branch";
+          c = "commit";
+          d = "diff";
+          l = "log";
+          m = "merge";
+          r = "rebase";
+          s = "status";
+          co = "checkout";
+          dt = "difftool";
+          ps = "push";
+          pl = "pull";
+          lol = "log --oneline";
+          amend = "commit --no-edit --amend";
+          cl = "clone";
+        };
+        extraConfig = {
+          pull = {
+            rebase = true;
+          };
+          push = {
+            autoSetupRemote = true;
+          };
+          init = {
+            defaultBranch = "main";
+          };
+        };
+        delta = {
+          enable = true;
+          options = {
+            features = "side-by-side line-numbers decorations";
+            whitespace-error-style = "22 reverse";
+            syntax-theme = "Monokai Extended";
+            navigation = true;
+            plus-style = "syntax #012800";
+            minus-style = "syntax #340001";
+            decorations = {
+              commit-decoration-style = "bold yellow box ul";
+              file-style = "bold yellow ul";
+              file-decoration-style = "none";
+            };
           };
         };
       };
