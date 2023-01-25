53da3862aaaebbb3cb9343196f48dea6567ee166
Adrian Ordonez <adrianord@gmail.com>
2023-01-25 16:11:40 -0600

    feat(systems/darwin): add rah

--- systems/darwin/default.nix
+++ systems/darwin/default.nix
@@ -1,4 +1,5 @@
 { bootstrap }:
 {
   gon = bootstrap (import ./gon.nix);
+  rah = bootstrap (import ./rah.nix);
 }
---
+++ systems/darwin/rah.nix
@@ -0,0 +1,83 @@
+{
+  user = {
+    name = "aordonez";
+    email = "adrianord@gmail.com";
+    fullname = "Adrian Ordonez";
+  };
+  host = {
+    arch = "aarch64";
+    os = "darwin";
+  };
+
+  modules = [
+    ({ lib, ... }: {
+      home._.programs.emacs.enable = true;
+      neon = {
+        languages = lib.neon.language.enableLanguages
+          {
+            vscode = true;
+            zsh = true;
+            neovim = true;
+            languages = [
+              "codeql"
+              "dotnet"
+              "go"
+              "nix"
+              "nodejs"
+              "protobuf"
+              "python"
+              "rust"
+              "vale"
+              "yaml"
+            ];
+          };
+        darwin = {
+          utils = {
+            enable = true;
+            includeUnfree = true;
+
+          };
+          configure = true;
+        };
+
+        programs = {
+          aws.enable = true;
+          alacritty.enable = true;
+          kitty.enable = true;
+          datree.enable = true;
+          direnv.enable = true;
+          discord.enable = false;
+          firefox.enable = true;
+          kubernetes.enable = true;
+          neovim = { enable = true; configure = false; };
+          pulumi.enable = true;
+          utils.enable = true;
+          vscode.enable = true;
+          zsh.enable = true;
+          git.enable = true;
+          terraform.enable = true;
+        };
+      };
+    })
+    {
+      homebrew = {
+        taps = [
+          "homebrew/cask-fonts"
+        ];
+        casks = [
+          "discord"
+          "font-jetbrains-mono-nerd-font"
+          "obsidian"
+          "spotify"
+          "microsoft-remote-desktop"
+          "bloomrpc"
+          "balenaetcher"
+          "topnotch"
+        ];
+        brews = [
+          "firefoxpwa"
+        ];
+      };
+    }
+  ];
+}
