1aaa21f58bcea0f17eb39421c2d525abd8377c5e
Adrian Ordonez <adrianord@gmail.com>
2023-02-03 15:56:04 -0600

    feat(programs/kitty): emulate Alt+x on Cmd+x for macos

--- modules/programs/kitty.nix
+++ modules/programs/kitty.nix
@@ -1,4 +1,4 @@
-{ lib, config, ... }:
+{ lib, config, pkgs, ... }:
 
 with lib;
 let
@@ -21,32 +21,57 @@ in
     };
   };
 
-  config = mkIf cfg.enable {
-    home._ = {
-      home.file.mocha_kitty = {
-        source = "${catppuccinRepo}/mocha.conf";
-        target = ".config/kitty/themes/mocha.conf";
-      };
-      programs.kitty = {
-        enable = true;
-        font = {
-          name = cfg.font;
-          size = 13;
+  config = mkIf cfg.enable (mkMerge [
+    ({
+      home._ = {
+        home.file.mocha_kitty = {
+          source = "${catppuccinRepo}/mocha.conf";
+          target = ".config/kitty/themes/mocha.conf";
         };
-        theme = "Catppuccin-Mocha";
-        settings = {
-          scrollback_lines = 10000;
-          enable_audio_bell = false;
-          update_check_interval = 0;
-          macos_titlebar_color = "background";
-          macos_quit_when_last_window_closed = "yes";
-          placement_strategy = "top-left";
-          cursor_blink_interval = 0;
-          tab_bar_edge = "top";
-          tab_bar_style = "slant";
-          disable_ligatures = "always";
+        programs.kitty = {
+          enable = true;
+          font = {
+            name = cfg.font;
+            size = 13;
+          };
+          theme = "Catppuccin-Mocha";
+          settings = {
+            clear_all_shortcuts = "yes";
+            scrollback_lines = 10000;
+            enable_audio_bell = false;
+            update_check_interval = 0;
+            macos_titlebar_color = "background";
+            macos_quit_when_last_window_closed = "yes";
+            placement_strategy = "top-left";
+            cursor_blink_interval = 0;
+            tab_bar_edge = "top";
+            tab_bar_style = "slant";
+            disable_ligatures = "always";
+          };
         };
       };
-    };
-  };
+    })
+
+    (mkIf pkgs.stdenv.hostPlatform.isDarwin {
+      home._.programs.kitty = {
+        # Helpful video https://www.youtube.com/watch?v=lHBD6pdJ-Ng
+        keybindings =
+          {
+            "cmd+n" = "new_os_window";
+            "cmd+c" = "copy_to_clipboard";
+            "cmd+v" = "paste_from_clipboard";
+            "cmd+enter" = "send_text all \\x1b\\r"; # Emulate Alt+Enter
+          } // mkMerge (genList
+            (x:
+              let
+                code = toString (x + 48);
+                num = toString x;
+              in
+              {
+                "cmd+ctrl+${num}" = "send_text all \\x1b[${code};7u"; # Emulate Alt+Ctrl+x
+                "cmd+${num}" = "send_text all \\x1b[${code};3u"; # Emulate Alt+x
+              }) 10);
+      };
+    })
+  ]);
 }
