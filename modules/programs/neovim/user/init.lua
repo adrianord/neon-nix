34639d2d1776a5d26b73ca986abd40a413738994
Adrian Ordonez <adrianord@gmail.com>
2023-02-10 21:59:47 -0600

    feat(programs/neovim): format init.lua

--- modules/programs/neovim/user/init.lua
+++ modules/programs/neovim/user/init.lua
@@ -3,78 +3,75 @@ local lspCodeAction = { function() vim.lsp.buf.code_action() end, desc = "LSP co
 local toggleExplorer = { "<cmd>Neotree toggle<cr>", desc = "Toggle Explorer" };
 
 return {
-  colorscheme = "catppuccin",
-
-  options = {
-    opt = {
-      relativenumber = true,
-      number = true,
-      spell = false,
-      signcolumn = "auto",
-      wrap = false,
-      autoindent = true,
+    colorscheme = "catppuccin",
+    options = {
+        opt = {
+            relativenumber = true,
+            number = true,
+            spell = false,
+            signcolumn = "auto",
+            wrap = false,
+            autoindent = true,
+        },
+        g = {
+            mapleader = " ",
+            autoformat_enabled = true,
+            cmp_enabled = true,
+            autopairs_enabled = true,
+            diagnostics_enabled = true,
+            status_diagnostics_enabled = true,
+            icons_enabled = true,
+            ui_notifications_enabled = true,
+            heirline_bufferline = false,
+        },
     },
-    g = {
-      mapleader = " ",
-      autoformat_enabled = true,
-      cmp_enabled = true,
-      autopairs_enabled = true,
-      diagnostics_enabled = true,
-      status_diagnostics_enabled = true,
-      icons_enabled = true,
-      ui_notifications_enabled = true,
-      heirline_bufferline = false,
+    mappings = {
+        n = {
+            ["<leader>th"] = toggleTerm,
+            ["<M-C-1>"] = toggleTerm,
+            ["<D-C-1>"] = toggleTerm,
+            ["<M-1>"] = toggleExplorer,
+            ["<D-1>"] = toggleExplorer,
+            ["<M-CR>"] = lspCodeAction,
+            ["<D-CR>"] = lspCodeAction,
+            ["<leader>q"] = { "<cmd>qa<cr>", desc = "Quit" }
+        },
+        i = {
+            ["<M-C-1>"] = toggleTerm,
+            ["<D-C-1>"] = toggleTerm,
+        },
+        t = {
+            ["<M-C-1>"] = toggleTerm,
+            ["<D-C-1>"] = toggleTerm,
+            ["<S-ESC>"] = toggleTerm,
+            ["<C-l>"] = false,
+        },
     },
-  },
-
-  mappings = {
-    n = {
-      ["<leader>th"] = toggleTerm,
-      ["<M-C-1>"] = toggleTerm,
-      ["<D-C-1>"] = toggleTerm,
-      ["<M-1>"] = toggleExplorer,
-      ["<D-1>"] = toggleExplorer,
-      ["<M-CR>"] = lspCodeAction,
-      ["<D-CR>"] = lspCodeAction,
-      ["<leader>q"] = { "<cmd>qa<cr>", desc = "Quit" }
+    plugins = {
+        init = {
+            { "catppuccin/nvim" },
+            { "gpanders/editorconfig.nvim" },
+            ["Shatur/neovim-session-manager"] = { event = "VimEnter" }
+        },
     },
-    i = {
-      ["<M-C-1>"] = toggleTerm,
-      ["<D-C-1>"] = toggleTerm,
-    },
-    t = {
-      ["<M-C-1>"] = toggleTerm,
-      ["<D-C-1>"] = toggleTerm,
-      ["<C-l>"] = false,
-    },
-  },
-
-  plugins = {
-    init = {
-      { "catppuccin/nvim" },
-      { "gpanders/editorconfig.nvim" },
-      ["Shatur/neovim-session-manager"] = { event = "VimEnter" }
-    },
-  },
-
-  polish = function()
-    local function alpha_on_bye(cmd)
-      local bufs = vim.fn.getbufinfo { buflisted = true }
-      vim.cmd(cmd)
-      if require("core.utils").is_available "alpha-nvim" and not bufs[2] then
-        require("alpha").start(true)
+    polish = function()
+      local function alpha_on_bye(cmd)
+        local bufs = vim.fn.getbufinfo { buflisted = true }
+        vim.cmd(cmd)
+        if require("core.utils").is_available "alpha-nvim" and not bufs[2] then
+          require("alpha").start(true)
+        end
       end
-    end
 
-    vim.keymap.del("n", "<leader>c")
-    if require("core.utils").is_available "bufdelete.nvim" then
-      vim.keymap.set("n", "<leader>c", function()
-        alpha_on_bye "Bdelete!"
-      end, { desc = "Close buffer" })
-    else
-      vim.keymap.set("n", "<leader>c", function()
-        alpha_on_bye "bdelete!"
-      end, { desc = "Close buffer" })
-    end
-  end,
+      vim.keymap.del("n", "<leader>c")
+      if require("core.utils").is_available "bufdelete.nvim" then
+        vim.keymap.set("n", "<leader>c", function()
+          alpha_on_bye "Bdelete!"
+        end, { desc = "Close buffer" })
+      else
+        vim.keymap.set("n", "<leader>c", function()
+          alpha_on_bye "bdelete!"
+        end, { desc = "Close buffer" })
+      end
+    end,
 }
