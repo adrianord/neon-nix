e129fc751e3c472566e3043ea1349931dcfdc3db
Adrian Ordonez <adrianord@gmail.com>
2023-05-17 23:10:22 -0400

    fix(program/neovim): change cmp select behavior to select on <C-j> and <C-k>

--- modules/programs/neovim/user/plugins/nvim-cmp.lua
+++ modules/programs/neovim/user/plugins/nvim-cmp.lua
@@ -28,13 +28,22 @@ local kind_icons = {
 
 return {
   "hrsh7th/nvim-cmp",
-  opts = {
-    formatting = {
-      format = function(_, vim_item)
-        -- Kind icons
-        vim_item.kind = string.format('%s', kind_icons[vim_item.kind]) -- This concatonates the icons with the name of the item kind
-        return vim_item
-      end
-    },
-  },
+  opts = (function()
+    local cmp = require "cmp"
+    return {
+      formatting = {
+        format = function(_, vim_item)
+          -- Kind icons
+          vim_item.kind = string.format('%s', kind_icons[vim_item.kind]) -- This concatonates the icons with the name of the item kind
+          return vim_item
+        end
+      },
+      mapping = {
+        ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
+        ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
+        ["<C-k>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
+        ["<C-j>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
+      },
+    }
+  end)()
 }
