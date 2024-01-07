35804f8adc5680e86851690dddea36d59436c9a8
Adrian Ordonez <adrianord@gmail.com>
2024-01-06 22:31:27 -0500

    fix(go): don't pin on version

--- modules/languages/go.nix
+++ modules/languages/go.nix
@@ -22,7 +22,7 @@ in
         programs = {
           go = {
             enable = true;
-            package = pkgs.go_1_20;
+            package = pkgs.go;
             inherit goPath goBin;
           };
         };
